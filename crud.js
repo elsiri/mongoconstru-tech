babe87d867b077f9fd7d2f13cf16bf9f5c127a0f
kevincastrillon70 <30646221+elsiri@users.noreply.github.com>
2023-06-07 02:10:24 -0500

    Se agregaro funciones
    Se agregaron operaciones crud para clientes y empleados

--- crud.js
+++ crud.js
@@ -7,7 +7,7 @@ const uri = 'mongodb+srv://kevincastrillon31:admin123@clusterkevin.lmjqevf.mongo
 /*-------------------------------------*/
 //Fin paso 1
 
-//Paso 2 crear funciones para agregar documentos en las colecciones de clientes, empleados y obras
+//Paso 2 funciones curd para agregar/actualizar/eliminar documentos en las colecciones de clientes, empleados y obras
 /*-------------------------------------*/
 //crear funcion asincrona para guardar clientes, recibiendo como parametro un array de objetos con propiedades de dicho objeto, debe realizarse con la funcion insertMany de mongodb
 async function agregarCliente(clienteNuevo){
@@ -26,6 +26,107 @@ async function agregarCliente(clienteNuevo){
 
         //se crea mensaje de guardado exitoso
         console.log(`Clientes agregados con exito`);
+    
+    } catch (error) {
+
+        //se crea mensaje de error en caso de que falle la conexion o el guardado del cliente
+        console.log(error);
+    
+    } finally {
+
+        //se cierra la conexion con base de datos por seguridad
+        await client.close();
+    
+    }
+
+}
+
+//crear funcion asincrona para obtener cliente, recibiendo como parametro un objeto con las propiedades que se requieran buscar las coincidencias
+async function buscarCliente(busqueda){
+
+    //se instancia conexion a mongodb con las credenciales 
+    const client = new MongoClient(uri);
+
+    //se crea try/catch/finally para hacer seguimiento de la funcion buscarCliente
+    try {
+
+        //se realiza conexion con await para garantizar que se conecte primero antes de realizar otra operacion
+        await client.connect();
+
+        //se realiza la busqueda de los clientes y se guarda el resultado del query en una variable
+        const result = await client.db('construtech').collection('clientes').find(busqueda).toArray();
+
+        //se crea mensaje de busqueda exitosa, se debe parsear el regultado a string ya que result es un objeto
+        console.log(`Clientes encontrados ${JSON.stringify(result)}, con el criterio de busqueda ${JSON.stringify(busqueda)}`);
+    
+    } catch (error) {
+
+        //se crea mensaje de error en caso de que falle la conexion o el guardado del cliente
+        console.log(error);
+    
+    } finally {
+
+        //se cierra la conexion con base de datos por seguridad
+        await client.close();
+    
+    }
+
+}
+
+//crear funcion asincrona para actualizar cliente, recibiendo 2 parametros, el primero es la condicion que se debe cumplir para realizar la actualizacion y la actualizacion que seria un objeto el $set y las propiedades del cliente que se requieren actualizar
+async function actualizarCliente(condicion, actualizacion){
+
+    //se instancia conexion a mongodb con las credenciales
+    const client = new MongoClient(uri);
+
+    //se crea try/catch/finally para hacer seguimiento de la funcion actualizarCliente
+    try {
+
+        //se realiza conexion con await para garantizar que se conecte primero antes de realizar otra operacion
+        await client.connect();
+
+        //se realiza la actualizacion del cliente y se guarda el resultado del query en una variable
+        const result = await client.db('construtech').collection('clientes').updateOne(condicion,actualizacion);
+
+        //se crea mensaje de actualizacion exitosa
+        console.log(`El cliente con criterio de actualizacion ${JSON.stringify(condicion)} fue actualizado con exito`);
+    
+    } catch (error) {
+
+        //se crea mensaje de error en caso de que falle la conexion o el guardado del cliente
+        console.log(error);
+    
+    } finally {
+
+        //se cierra la conexion con base de datos por seguridad
+        await client.close();
+    
+    }
+
+}
+
+//crear funcion asincrona para eliminar cliente, recibiendo 1 parametro que es la condicion que se debe cumplir para eliminar el cliente
+async function eliminarCliente(condicion){
+
+    //se instancia conexion a mongodb con las credenciales
+    const client = new MongoClient(uri);
+
+    //se crea try/catch/finally para hacer seguimiento de la funcion eliminarCliente
+    try {
+
+        //se realiza conexion con await para garantizar que se conecte primero antes de realizar otra operacion
+        await client.connect();
+
+        //se realiza la elimiacion del cliente y se guarda el resultado del query en una variable
+        const result = await client.db('construtech').collection('clientes').deleteOne(condicion);
+
+        //se crea if con validacion, se valida si hubo al menos una eliminacion y si hay una respuesta true, luego se crea mensaje de eliminado exitoso o de error
+        if (result.acknowledged == true && result.deletedCount >= 1) {
+            console.log(`El cliente con criterio de busqueda ${JSON.stringify(condicion)} fue eliminado con exito`);            
+        } else {
+            console.log("Error al intentar eliminar cliente con criterio de busqueda "+JSON.stringify(condicion))
+        }
+
     
     } catch (error) {
 
@@ -99,6 +200,106 @@ async function agregarEmpleado(empleadoNuevo){
 
 }
 
+//crear funcion asincrona para obtener empleado, recibiendo como parametro un objeto con las propiedades que se requieran buscar las coincidencias
+async function buscarEmpleado(busqueda){
+
+    //se instancia conexion a mongodb con las credenciales 
+    const client = new MongoClient(uri);
+
+    //se crea try/catch/finally para hacer seguimiento de la funcion buscarEmpleado
+    try {
+
+        //se realiza conexion con await para garantizar que se conecte primero antes de realizar otra operacion
+        await client.connect();
+
+        //se realiza la busqueda de los empleados y se guarda el resultado del query en una variable
+        const result = await client.db('construtech').collection('empleados').find(busqueda).toArray();
+
+        //se crea mensaje de busqueda exitosa, se debe parsear el regultado a string ya que result es un objeto
+        console.log(`empleados encontrados ${JSON.stringify(result)}, con el criterio de busqueda ${JSON.stringify(busqueda)}`);
+    
+    } catch (error) {
+
+        //se crea mensaje de error en caso de que falle la conexion o el guardado del empleado
+        console.log(error);
+    
+    } finally {
+
+        //se cierra la conexion con base de datos por seguridad
+        await client.close();
+    
+    }
+
+}
+
+//crear funcion asincrona para actualizar empleado, recibiendo 2 parametros, el primero es la condicion que se debe cumplir para realizar la actualizacion y la actualizacion que seria un objeto el $set y las propiedades del empleado que se requieren actualizar
+async function actualizarEmpleado(condicion, actualizacion){
+
+    //se instancia conexion a mongodb con las credenciales
+    const client = new MongoClient(uri);
+
+    //se crea try/catch/finally para hacer seguimiento de la funcion actualizarEmpleado
+    try {
+
+        //se realiza conexion con await para garantizar que se conecte primero antes de realizar otra operacion
+        await client.connect();
+
+        //se realiza la actualizacion del empleado y se guarda el resultado del query en una variable
+        const result = await client.db('construtech').collection('empleados').updateOne(condicion,actualizacion);
+
+        //se crea mensaje de actualizacion exitosa
+        console.log(`El empleado con criterio de actualizacion ${JSON.stringify(condicion)} fue actualizado con exito`);
+    
+    } catch (error) {
+
+        //se crea mensaje de error en caso de que falle la conexion o el guardado del empleado
+        console.log(error);
+    
+    } finally {
+
+        //se cierra la conexion con base de datos por seguridad
+        await client.close();
+    
+    }
+
+}
+
+//crear funcion asincrona para eliminar empleado, recibiendo 1 parametro que es la condicion que se debe cumplir para eliminar el empleado
+async function eliminarEmpleado(condicion){
+
+    //se instancia conexion a mongodb con las credenciales
+    const client = new MongoClient(uri);
+
+    //se crea try/catch/finally para hacer seguimiento de la funcion eliminarEmpleado
+    try {
+
+        //se realiza conexion con await para garantizar que se conecte primero antes de realizar otra operacion
+        await client.connect();
+
+        //se realiza la elimiacion del empleado y se guarda el resultado del query en una variable
+        const result = await client.db('construtech').collection('empleados').deleteOne(condicion);
+
+        //se crea if con validacion, se valida si hubo al menos una eliminacion y si hay una respuesta true, luego se crea mensaje de eliminado exitoso o de error
+        if (result.acknowledged == true && result.deletedCount >= 1) {
+            console.log(`El empleado con criterio de busqueda ${JSON.stringify(condicion)} fue eliminado con exito`);            
+        } else {
+            console.log("Error al intentar eliminar empleado con criterio de busqueda "+JSON.stringify(condicion))
+        }
+
+    
+    } catch (error) {
+
+        //se crea mensaje de error en caso de que falle la conexion o el guardado del empleado
+        console.log(error);
+    
+    } finally {
+
+        //se cierra la conexion con base de datos por seguridad
+        await client.close();
+    
+    }
+
+}
 
 //Se crea funcion asincrona para insertar el id de cada empleado en una obra
 async function setIdEmpleados(){
@@ -163,7 +364,13 @@ async function setIdClientes(){
 module.exports ={
     agregarObra,
     agregarCliente,
+    buscarCliente,
+    actualizarCliente,
+    eliminarCliente,
     agregarEmpleado,
+    buscarEmpleado,
+    actualizarEmpleado,
+    eliminarEmpleado,
     setIdClientes,
     setIdEmpleados
 }
--- index.js
+++ index.js
@@ -10,83 +10,125 @@ const {faker} = require("@faker-js/faker");
 
 //Paso 2  
 /*-------------------------------------*/
-//se crea array de clientes
-const array_clientes = []; 
-// //crear bucle para poblar la coleccion de clientes
-for (let i = 0; i < 2010; i++) {
-    //crear array con estados validos para asignarlos a cada cliente
-    const estados = ["activo","inactivo"];
-    //crear un random entre los indices que existe en el array de estados para generar uno aleatoriamente cuando se guarda un cliente
-    const random = Math.floor(Math.random() * estados.length);
+// //se crea array de clientes
+// const array_clientes = []; 
+// // //crear bucle para poblar la coleccion de clientes
+// for (let i = 0; i < 2010; i++) {
+//     //crear array con estados validos para asignarlos a cada cliente
+//     const estados = ["activo","inactivo"];
+//     //crear un random entre los indices que existe en el array de estados para generar uno aleatoriamente cuando se guarda un cliente
+//     const random = Math.floor(Math.random() * estados.length);
 
-    //se crea un objeto cliente para definir sus propiedades requeridas en las validaciones de la coleccion, estas propiedades se crean con la libreria faker
-    cliente ={
-        nombres: faker.person.firstName(),
-        apellidos: faker.person.lastName(),
-        //se asigna estado creado aleatoriamente
-        estado: estados[random],
-        //formato de numero
-        telefono:faker.phone.number('3##-###-##-##'),
-        direccion:faker.location.streetAddress({ useFullAddress: true })
-    }
-    //se añade el objeto cliente creado dentro del bucle al array de clientes
-    array_clientes.push(cliente);
-}  
-//se envia el array de clientes a la funcion exportada agregarCliente
-operaciones.agregarCliente(array_clientes);
+//     //se crea un objeto cliente para definir sus propiedades requeridas en las validaciones de la coleccion, estas propiedades se crean con la libreria faker
+//     cliente ={
+//         nombres: faker.person.firstName(),
+//         apellidos: faker.person.lastName(),
+//         //se asigna estado creado aleatoriamente
+//         estado: estados[random],
+//         //formato de numero
+//         telefono:faker.phone.number('3##-###-##-##'),
+//         direccion:faker.location.streetAddress({ useFullAddress: true })
+//     }
+//     //se añade el objeto cliente creado dentro del bucle al array de clientes
+//     array_clientes.push(cliente);
+// }  
+// //se envia el array de clientes a la funcion exportada agregarCliente
+// operaciones.agregarCliente(array_clientes);
 
-//se crea array de empleados
-const array_empleados =[];
-//crear bucle para poblar coleccion de empleados
-for (let i = 0; i < 2010;) {
-    //crear array con estados validos para asignarlos a cada empleado
-    const estados = ["activo","inactivo"];
-    i++;
-    //crear un random entre los indices que existe en el array de estados para generar uno aleatoriamente cuando se guarda un empleado
-    const random = Math.floor(Math.random() * estados.length);
+// //se crea array de empleados
+// const array_empleados =[];
+// //crear bucle para poblar coleccion de empleados
+// for (let i = 0; i < 2010;) {
+//     //crear array con estados validos para asignarlos a cada empleado
+//     const estados = ["activo","inactivo"];
+//     i++;
+//     //crear un random entre los indices que existe en el array de estados para generar uno aleatoriamente cuando se guarda un empleado
+//     const random = Math.floor(Math.random() * estados.length);
 
-    //se crea un objeto empleado para definir sus propiedades requeridas en las validaciones de la coleccion, estas propiedades se crean con la libreria faker
-    empleado ={
-        nombres: faker.person.firstName()+" "+faker.person.lastName(),
-        cedula: [faker.helpers.arrayElement(['CC', 'CE', 'CNV', 'P', "PT"]), faker.number.int()],
-        //se asigna estado creado aleatoriamente
-        estado: estados[random],
-        //formato de numero
-        telefono:"'"+faker.phone.number('3##-###-##-##')+"'",
-        direccion:[faker.location.city(), faker.location.streetAddress({ useFullAddress: true })]
-    }
-    //se añade el objeto empleado creado dentro del bucle al array de empleados
-    array_empleados.push(empleado)
-}
-//se envia el array de empleados a la funcion exportada agregarEmpleado
-operaciones.agregarEmpleado(array_empleados)
+//     //se crea un objeto empleado para definir sus propiedades requeridas en las validaciones de la coleccion, estas propiedades se crean con la libreria faker
+//     empleado ={
+//         nombres: faker.person.firstName()+" "+faker.person.lastName(),
+//         cedula: [faker.helpers.arrayElement(['CC', 'CE', 'CNV', 'P', "PT"]), faker.number.int()],
+//         //se asigna estado creado aleatoriamente
+//         estado: estados[random],
+//         //formato de numero
+//         telefono:"'"+faker.phone.number('3##-###-##-##')+"'",
+//         direccion:[faker.location.city(), faker.location.streetAddress({ useFullAddress: true })]
+//     }
+//     //se añade el objeto empleado creado dentro del bucle al array de empleados
+//     array_empleados.push(empleado)
+// }
+// //se envia el array de empleados a la funcion exportada agregarEmpleado
+// operaciones.agregarEmpleado(array_empleados)
 
-//se crea array de obras
-const array_obras= []
-//crear bucle para poblar coleccion de obras
-for (let i = 0; i < 2010; i++) {
-        //crear array con estados validos para asignarlos a cada obra
-        const estados = ["activo","inactivo"];
+// //se crea array de obras
+// const array_obras= []
+// //crear bucle para poblar coleccion de obras
+// for (let i = 0; i < 2010; i++) {
+//         //crear array con estados validos para asignarlos a cada obra
+//         const estados = ["activo","inactivo"];
         
-        //crear un random entre los indices que existe en el array de estados para generar uno aleatoriamente cuando se guarda un obra
-        const random = Math.floor(Math.random() * estados.length);
-    //se crea un objeto empleado para definir sus propiedades requeridas en las validaciones de la coleccion, estas propiedades se crean con la libreria faker
-    obra = {
-        //se crean campos de llaves foraneas de empleados y clientes, para guardarlas posteriormente
-        idEmpl:"",
-        idCli:"",
-        descripcion: faker.helpers.arrayElement(['Construccion con Drywall', 'Reparacion de plomeria', 'Instalacion de gas', 'Pintar fachada', "Estucado"]),
-        fecha: ["'"+faker.date.past()+"'","'"+faker.date.recent()+"'"],
-        //se asigna estado creado aleatoriamente
-        estado: estados[random],
-        //formato de numero
-        cantidad:faker.number.bigInt(),
-    }
-    //se añade el objeto obra creado dentro del bucle al array de obras
-    array_obras.push(obra)
-}
-//se envia el array de obras a la funcion exportada agregarObra
-operaciones.agregarObra(array_obras)
+//         //crear un random entre los indices que existe en el array de estados para generar uno aleatoriamente cuando se guarda un obra
+//         const random = Math.floor(Math.random() * estados.length);
+//     //se crea un objeto empleado para definir sus propiedades requeridas en las validaciones de la coleccion, estas propiedades se crean con la libreria faker
+//     obra = {
+//         //se crean campos de llaves foraneas de empleados y clientes, para guardarlas posteriormente
+//         idEmpl:"",
+//         idCli:"",
+//         descripcion: faker.helpers.arrayElement(['Construccion con Drywall', 'Reparacion de plomeria', 'Instalacion de gas', 'Pintar fachada', "Estucado"]),
+//         fecha: ["'"+faker.date.past()+"'","'"+faker.date.recent()+"'"],
+//         //se asigna estado creado aleatoriamente
+//         estado: estados[random],
+//         //formato de numero
+//         cantidad:faker.number.bigInt(),
+//     }
+//     //se añade el objeto obra creado dentro del bucle al array de obras
+//     array_obras.push(obra)
+// }
+// //se envia el array de obras a la funcion exportada agregarObra
+// operaciones.agregarObra(array_obras)
+
+/*----------------Operaciones crud clientes---------------------*/
+
+//actualizar cliente, esta funcion recibe 2 parametros, el primero es la condicion para realizar el update y el segundo son los campos que se actualizaran 
+//variable condicion de tipo objeto para funcion updateCliente, esta funciona como condicion para la actualizacion
+// let condicion = {"nombres" : "Chanelle"};
+// //variable actualizacion de tipo objeto para funcion actualizarCliente, esta funciona para incluir las propiedades que se deseen actualizar si se cumple la condicion anterior
+// let actualizacion  = {$set: {"nombres": "Anna"}}
+// operaciones.actualizarCliente(condicion,actualizacion)
+
+
+// //buscar cliente, esta funcion recibe 1 parametro, este parametro debe contener las coincidencias que se quieren encontrar en la coleccion de clientes
+// //variable busqueda de tipo objeto para funcion buscarCliente, esta funciona como criterio de busqueda de un cliente
+// let busqueda = {"nombres" : "Anna"};
+// operaciones.buscarCliente(busqueda)
+
+
+//variable condicion de tipo objeto para funcion eliminarCliente, esta funciona como condicion para elimminar
+// let condicion = {"nombres" : "Anna"};
+// //eliminar cliente, esta funcion recibe 1 parametro la condicion para eliminar un cliente que cumpla con dicha condicion 
+// operaciones.eliminarCliente(condicion)
+/*----------------Operaciones crud empleados---------------------*/
+
+// actualizar empleado, esta funcion recibe 2 parametros, el primero es la condicion para realizar el update y el segundo son los campos que se actualizaran 
+// variable condicion de tipo objeto para funcion actualizarEmpleado, esta funciona como condicion para la actualizacion
+// let condicion = {"nombres" : "Jaime Schulist"};
+// //variable actualizacion de tipo objeto para funcion actualizarEmpleado, esta funciona para incluir las propiedades que se deseen actualizar si se cumple la condicion anterior
+// let actualizacion  = {$set: {"nombres": "Javier"}}
+// operaciones.actualizarEmpleado(condicion,actualizacion)
+
+
+// //buscar empleado, esta funcion recibe 1 parametro, este parametro debe contener las coincidencias que se quieren encontrar en la coleccion de empleados
+// //variable busqueda de tipo objeto para funcion buscarEmpleado, esta funciona como criterio de busqueda de un empleado
+// let busqueda = {"nombres" : "Javier"};
+// operaciones.buscarEmpleado(busqueda)
+
+
+//variable condicion de tipo objeto para funcion eliminarEmpleado, esta funciona como condicion para elimminar
+let condicion = {"nombres" : "Javier"};
+//eliminar empleado, esta funcion recibe 1 parametro la condicion para eliminar un empleado que cumpla con dicha condicion 
+operaciones.eliminarEmpleado(condicion)
+
 /*-------------------------------------*/
 //Fin paso 1
 
@@ -99,3 +141,4 @@ operaciones.agregarObra(array_obras)
 // operaciones.setIdClientes();
 
 //Paso 3 (opcional) en caso de requerir realizar funciones de aggregate como $lookup ve al archivo comandos mongo que se encuentra en la carpeta "mongo_background", las funciones de aggregate estan en el paso 4 de ese archivo
+
